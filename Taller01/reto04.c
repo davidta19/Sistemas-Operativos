eb72d7f34fdced8923974c69df422eab9f06605f
davidta19 <davidtobara11@gmail.com>
2025-08-21 13:26:26 -0500

    reto04.c

--- Taller01/reto04.c
+++ Taller01/reto04.c
@@ -1,1 +1,38 @@
+/**************************************************************************************
+ * Fecha:21/08/2025
+ * Autor: David Tobar Artunduaga
+ * Materia: Sistemas Operativos
+ * Tema: Memoria dinamica
+ **************************************************************************************/
+
+#include <stdio.h>  
+#include <stdlib.h>  
+
+int main() {
+    int n, i, *ptr, sum = 0;                // Defino variables y apuntadores de tipo entero
+
+    printf("Enter number of elements: ");   // pido al usuario cuántos elementos quiere
+    scanf("%d", &n);                        // guardo ese número en n
+
+    ptr = (int*) calloc(n, sizeof(int));    // reservo memoria para n enteros y los inicializo en 0
+
+    if(ptr == NULL) {                       // si calloc no pudo asignar memoria
+        printf("Error! memory not allocated."); // muestro error
+        exit(0);                            // cierro el programa
+    }
+
+    printf("Enter elements: ");             // pido que ingrese los elementos
+
+    for(i = 0; i < n; ++i) {                // recorro desde 0 hasta n-1
+        scanf("%d", ptr + i);               // guardo el valor ingresado en la posición i usando punteros
+        sum += *(ptr + i);                  // sumo el valor recién ingresado al acumulador
+    }
+
+    printf("Sum = %d", sum);                // imprimo la suma de todos los elementos
+
+    
+
+    return 0;                               
+}
+
 
