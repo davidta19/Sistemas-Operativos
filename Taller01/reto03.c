de6f43933e73a5a0318fa374e44ee0eb06b22245
davidta19 <davidtobara11@gmail.com>
2025-08-21 13:21:34 -0500

    reto03.c

--- Taller01/reto03.c
+++ Taller01/reto03.c
@@ -1,1 +1,22 @@
+/**************************************************************************************
+ * Fecha:21/08/2025
+ * Autor: David Tobar Artunduaga
+ * Materia: Sistemas Operativos
+ * Tema: Memoria dinamica
+ **************************************************************************************/
+
+#include <stdio.h>  
+#include <stdlib.h>  
+
+int main() {
+    int *ptr;                           // puntero a entero
+    ptr = malloc(15 * sizeof(*ptr));    // reservo espacio para 15 enteros en memoria dinámica
+
+    if (ptr != NULL) {                  // verifico que malloc no devolvió NULL
+        *(ptr + 5) = 480;               // asigno 480 al sexto entero 
+        printf("Value of the 6th integer is %d\n", *(ptr + 5)); // imprimo el valor almacenado en la posición 6                
+    }
+
+    return 0;                           
+}
 
